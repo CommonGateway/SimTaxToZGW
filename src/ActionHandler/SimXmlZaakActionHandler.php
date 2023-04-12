bd6eb39bd967a427393b4d6f7e0bbfa78ef1e4c3
Wilco Louwerse <wilco@conduction.nl>
2023-04-12 15:59:27 +0200

    Added & changed some copy paste stuff from VrijBRP bundles for xml->zgw

---
+++ src/ActionHandler/SimXmlZaakActionHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CommonGateway\NaamgebruikVrijBRPBundle\ActionHandler;
+
+use CommonGateway\CoreBundle\ActionHandler\ActionHandlerInterface;
+use CommonGateway\NaamgebruikVrijBRPBundle\Service\SimXmlToZgwService;
+use CommonGateway\NaamgebruikVrijBRPBundle\Service\ZdsToZgwService;
+
+/**
+ * Convert Sim Xml to a ZGW Zaak.
+ */
+class SimXmlZaakActionHandler implements ActionHandlerInterface
+{
+    /**
+     * @var SimXmlToZgwService
+     */
+    private SimXmlToZgwService $simXmlToZgwService;
+
+    /**
+     * @param SimXmlToZgwService $simXmlToZgwService The Sim XML to ZGW service
+     */
+    public function __construct(SimXmlToZgwService $simXmlToZgwService)
+    {
+        $this->simXmlToZgwService = $simXmlToZgwService;
+    }//end __construct()
+
+    /**
+     *  This function returns the required configuration as a [json-schema](https://json-schema.org/) array.
+     *
+     * @return array a [json-schema](https://json-schema.org/) that this  action should comply to
+     */
+    public function getConfiguration(): array
+    {
+        return [
+            '$id'         => 'https://simxml.nl/ActionHandler/SimXmlZaakActionHandler.ActionHandler.json',
+            '$schema'     => 'https://docs.commongateway.nl/schemas/ActionHandler.schema.json',
+            'title'       => 'SimXmlZaakActionHandler',
+            'description' => 'This handler converts Sim Xml to a ZGW Zaak.',
+            'required'    => [],
+            'properties'  => [],
+        ];
+    }//end getConfiguration()
+
+    /**
+     * This function runs the service.
+     *
+     * @param array $data          The data from the call
+     * @param array $configuration The configuration of the action
+     *
+     * @return array
+     */
+    public function run(array $data, array $configuration): array
+    {
+        return $this->simXmlToZgwService->zaakActionHandler($data, $configuration);
+    }//end run()
+}
