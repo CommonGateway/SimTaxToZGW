70bbc6901fb5e194359fbb279bd4e1ab5ef711c2
Wilco Louwerse <wilco@conduction.nl>
2023-04-14 12:14:03 +0200

    Fix belastingplichtige.burgerservicenummer filter for getAanslagen

--- src/Service/SimTaxService.php
+++ src/Service/SimTaxService.php
@@ -165,9 +165,9 @@ class SimTaxService
         }
 
         $filter = [];
-        if (isset($vraagBericht['ns2:body']['ns2:ABT']['ns2:ABTSUBANV']['ns2:PRS']['ns2:bsn-nummer']) === true) {
-            $filter['belastingplichtige.burgerservicenummer'] =
-                $vraagBericht['ns2:body']['ns2:ABT']['ns2:ABTSUBANV']['ns2:PRS']['ns2:bsn-nummer'];
+        if (isset($vraagBericht['ns2:body']['ns2:ABT'][0]['ns2:ABTSUBANV']['ns2:PRS']['ns2:bsn-nummer']) === true) {
+            $filter['embedded.belastingplichtige.burgerservicenummer'] =
+                $vraagBericht['ns2:body']['ns2:ABT'][0]['ns2:ABTSUBANV']['ns2:PRS']['ns2:bsn-nummer'];
         }
 
         $aanslagen = $this->cacheService->searchObjects(null, $filter, [$this::SCHEMA_REFS['Aanslagbiljet']]);
